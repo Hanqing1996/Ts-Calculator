466909f56eb692e4f073a85a5f33507446270c1f
zhq <zhq199622@163.com>
2020-02-07 11:37:13 +0800

    增加 output

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
-function createButton(element) {
+function createButton(element, className) {
     var button = document.createElement('button');
+    button.classList.add(className);
+    button.classList.add('button');
     button.textContent = element;
     return button;
 }
@@ -14,11 +16,22 @@ window.addEventListener('load', function () {
     var container = document.createElement('div');
     container.classList.add('container');
     document.body.appendChild(container);
+    var output = document.createElement('div');
+    output.classList.add('output');
+    container.appendChild(output);
+    var span = document.createElement('span');
+    span.classList.add('result');
+    span.textContent = '0';
+    output.appendChild(span);
+    container.addEventListener('click', function (e) {
+        console.log(e.target);
+    });
     strs.forEach(function (array) {
         var rowDiv = document.createElement('div');
         rowDiv.classList.add('row');
         array.forEach(function (element) {
-            rowDiv.appendChild(createButton(element));
+            var button = createButton(element, "text-" + element);
+            rowDiv.appendChild(button);
         });
         container.appendChild(rowDiv);
     });
--- calculator.ts
+++ calculator.ts
@@ -1,9 +1,10 @@
-function createButton(element: string):HTMLButtonElement {
+function createButton(element: string,className:string):HTMLButtonElement {
     let button: HTMLButtonElement = document.createElement('button')
+    button.classList.add(className)
+    button.classList.add('button')
     button.textContent = element
     return button
 }
-
 
 let strs: Array<Array<string>> =
     [
@@ -14,18 +15,31 @@ let strs: Array<Array<string>> =
         ['0', '.', '='],
     ]
 
-
 window.addEventListener('load', () => {
 
     let container:HTMLDivElement=document.createElement('div') 
     container.classList.add('container')
     document.body.appendChild(container)
 
+    let output:HTMLDivElement=document.createElement('div') 
+    output.classList.add('output')
+    container.appendChild(output)
+
+    let span:HTMLSpanElement=document.createElement('span') 
+    span.classList.add('result')
+    span.textContent='0'
+    output.appendChild(span)
+
+    container.addEventListener('click',(e)=>{
+        console.log(e.target)
+    })
+
     strs.forEach(array => {
         let rowDiv:HTMLDivElement=document.createElement('div')
         rowDiv.classList.add('row')
         array.forEach(element => {
-            rowDiv.appendChild(createButton(element))
+            let button:HTMLButtonElement=createButton(element,`text-${element}`)
+            rowDiv.appendChild(button)
         })
         container.appendChild(rowDiv)
     })
